80f698ed0acf1db94b7fab5348b03f1b24ac9500
Jake <manumoomaw@gmail.com>
2022-12-02 22:52:14 -0800

    added guessing game

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -2,6 +2,7 @@
 
 use std::io; // standard library
 use rand::Rng;
+use std::cmp::Ordering;
 
 
 fn main() {
@@ -13,18 +14,35 @@ fn main() {
     // create a variable
     println!("the secret number is: {secret_number}");
 
-    println!("please input your guess");
 
-    let mut guess = String::new();
 
     let _apples = 5;  // immutable
     // let mut bananas = 5;    //mutable
+    // infinite loop
+    loop {
+        // convert string to int 
+        let mut guess = String::new();
 
+        println!("please input your guess");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read line");
+        let guess: u32 = guess.trim().parse().expect("Not a number");
+        // let guess: u32 = match guess.trim().parse() {
+        //     Ok(num) => num,
+        //     Err(_) => continue,
+        // };
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("failed to read line");
-    
-    println!("you guessed: {guess}");
+        println!("you guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("too small!"),
+            Ordering::Greater => println!("Too Big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 
 }
